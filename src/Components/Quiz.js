46750617719bbb31729b6992e35bd003c7aeb307
Teddi_r <ferryaja20000@gmail.com>
2022-10-30 13:41:54 +0700

    update UI UX and validasi show question + score

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,49 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom/dist";
 
-const Quiz = ({questions, category, name, difficulty, amount}) => {
-    const [ currentQuiz, setCurrentQuiz ] = useState(0);
-    const [ score, setScore ] = useState(0);
-    const [ showScore, setShowScore ] = useState(false);
-    const [ color, setColor ] = useState(false)
+const Quiz = ({ questions, category, name, difficulty, amount }) => {
+  const [currentQuiz, setCurrentQuiz] = useState(0);
+  const [score, setScore] = useState(0);
+  const navigation = useNavigate();
+  const [myAnswer, setMyAnswer] = useState("");
 
+  const checkAnswers = (correct) => {
+    setMyAnswer(correct);
+    if (questions?.[currentQuiz]?.correct_answer === correct) {
+      setScore(score + 1);
+    } 
+  };
 
-    const checkAnswers = (correct) => {
-        if (!showScore) {
-            if (correct === questions[currentQuiz].correct_answer) {
-                setScore(score + 1);
-                setColor(true)
-            } else {
-                setColor(false);
-            }
-        }
-        setShowScore(true);
-    }
+  const handleNextButton = () => {
+    setCurrentQuiz(currentQuiz + 1);
+  };
 
+  // console.log(questions?.[currentQuiz]?.correct_answer);
+  // console.log(questions);
 
-    const handleNextButton = () => {
-        setCurrentQuiz(currentQuiz + 1)
-        setShowScore(false);
-    }
-
-
-    return(
+  return (
+    <div className="p-4">
+      <div>
+        <h2 className={currentQuiz >= questions.length ? "block" : "hidden"}>
+          ur score out {score} of {questions.length}
+        </h2>
         <div>
-            <div>
-            <h2 className={currentQuiz >= questions.length ? 'block' : 'hidden'}>ur score out {score} of {questions.length}</h2>
-            <div>
-                <h2>{currentQuiz + 1}/{questions.length}</h2>
-                <h2>{questions[currentQuiz].question}</h2>
-                {questions[currentQuiz].answers.map((question) => (
-                    <h2 className={color ? "text-green-500" : "black"} onClick={() => checkAnswers(question)}>{question}</h2>
-                ))}
-                {showScore && (
-                    <button onClick={handleNextButton}>Next</button>
-                )}
-            </div>
-    </div>
-
+          {currentQuiz >= questions.length ? (
+            <>
+              <div className="flex-row flex">
+                <div className="mt-4 mr-2" onClick={() => {
+                  setCurrentQuiz(0)
+                  setScore(0)
+                }}>
+                  <span className="bg-pink-400 py-2 px-4 cursor-pointer">
+                    Ulangi
+                  </span>
+                </div>
+                <div className="mt-4" onClick={() => navigation("/")}>
+                  <span className="bg-pink-400 py-2 px-4 cursor-pointer">
+                    Pilih pertanyaan lain
+                  </span>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <h2>
+                {currentQuiz + 1}/{questions.length}
+              </h2>
+              <h2>{questions?.[currentQuiz]?.question}</h2>
+              {questions?.[currentQuiz]?.answers.map((question) => (
+                <h2
+                  className={`${myAnswer === question ? "text-green-500" : "black"} cursor-pointer`}
+                  onClick={() => checkAnswers(question)}
+                >
+                  {question}
+                </h2>
+              ))}
+                <div className="mt-4" onClick={() => myAnswer ? handleNextButton() : {}}>
+                  <span className={`${myAnswer ? 'bg-pink-400 text-white cursor-pointer' : 'bg-gray-300 text-gray-500 cursor-not-allowed'} py-2 px-4`}>
+                    Next
+                  </span>
+                </div>
+            </>
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Quiz;
